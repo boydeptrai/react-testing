ac979b394e724a88668ec2e65d30190f5584fbbe
boydeptrai <langthamht047@gmail.com>
2023-03-31 15:54:51 +0700

    filter test

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,15 +1,18 @@
 import { render,screen } from "@testing-library/react";
 import Greet from "./Greet";
 
-test('Greet renders correctly',() =>{
-    render(<Greet  />)
+describe('Greet', () =>{
 
-    const textElement = screen.getByText('Hello')
-    expect(textElement).toBeInTheDocument()
+    test('Greet renders correctly',() =>{
+        render(<Greet  />)
+    
+        const textElement = screen.getByText('Hello')
+        expect(textElement).toBeInTheDocument()
+    })
+    
+    test ('Greet render with a name',()=>{
+        render(<Greet name="tuan"/>)
+        const textElement = screen.getByText('Hello tuan')
+        expect(textElement).toBeInTheDocument()
 })
-
-test ('Greet render with a name',()=>{
-    render(<Greet name="tuan"/>)
-    const textElement = screen.getByText('Hello tuan')
-    expect(textElement).toBeInTheDocument()
 })
